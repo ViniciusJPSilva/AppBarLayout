b93336018503d309e1c044ce88779009373c5f95
Vinícius Pires <viniciusjps15@gmail.com>
2025-01-12 01:06:53 -0300

    Add main menu

--- app/src/main/java/tsi/pdmsf/appbarlayout/MainActivity.java
+++ app/src/main/java/tsi/pdmsf/appbarlayout/MainActivity.java
@@ -3,9 +3,13 @@ package tsi.pdmsf.appbarlayout;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -24,15 +28,41 @@ public class MainActivity extends AppCompatActivity {
             getSupportActionBar().setTitle(R.string.title);
 
         // Botão flutuante
-        findViewById(R.id.fab_open_website).setOnClickListener(view -> {
-            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getResources().getString(R.string.official_website_link)));
-            startActivity(browserIntent);
-        });
+        findViewById(R.id.fab_open_website).setOnClickListener(view -> openWebsite(getResources().getString(R.string.official_website_link)));
+
+        // Atividade: menu
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.main_menu);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.itemMenu1) {
+            openWebsite(getResources().getString(R.string.steam_link));
+            return true;
+        } else if (id == R.id.itemMenu2) {
+            openWebsite(getResources().getString(R.string.epic_link));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void openWebsite(String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
     }
 }
